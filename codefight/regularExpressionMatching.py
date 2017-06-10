6c6e4cfd29b7e312be3e9c442479a9b188704d82
SungGeun Kim <snugkim@gmail.com>
2017-06-10 11:28:14 -0700

    update: Adds explanation for DP solution of Regex

--- codefight/regularExpressionMatching.py
+++ codefight/regularExpressionMatching.py
@@ -59,7 +59,18 @@ def regularExpressionMatching(s, p):
         
 
 def regularExpressionMatchingDP(s, p):
-
+'''
+reference: https://discuss.leetcode.com/topic/40371/easy-dp-java-solution-with-detailed-explanation
+1, If p.charAt(j) == s.charAt(i) :  dp[i][j] = dp[i-1][j-1];
+2, If p.charAt(j) == '.' : dp[i][j] = dp[i-1][j-1];
+3, If p.charAt(j) == '*': 
+   here are two sub conditions:
+               1   if p.charAt(j-1) != s.charAt(i) : dp[i][j] = dp[i][j-2]  //in this case, a* only counts as empty
+               2   if p.charAt(i-1) == s.charAt(i) or p.charAt(i-1) == '.':
+                              dp[i][j] = dp[i-1][j]    //in this case, a* counts as multiple a 
+                           or dp[i][j] = dp[i][j-1]   // in this case, a* counts as single a
+                           or dp[i][j] = dp[i][j-2]   // in this case, a* counts as empty
+'''
     dp = [ [False for _ in range(len(p) + 1)] for _ in range(len(s) + 1) ]
     dp[0][0] = True
 
