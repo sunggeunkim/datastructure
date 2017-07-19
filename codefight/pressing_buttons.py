a45e2c171408b00a505323ac7f2ece761c5f55b0
SungGeun Kim <snugkim@gmail.com>
2017-07-19 09:59:53 -0700

    Update pressing_buttons.py

--- codefight/pressing_buttons.py
+++ codefight/pressing_buttons.py
@@ -1,6 +1,7 @@
 '''
 reference - https://codefights.com/interview-practice/task/SyBbdPYC86RdkKvjH
-Given a string of digits, return all of the possible non-empty letter combinations that the number could represent. The mapping of digits to letters is the same as you would find on a telephone's buttons, as in the example below:
+Given a string of digits, return all of the possible non-empty letter combinations that the number could represent.
+The mapping of digits to letters is the same as you would find on a telephone's buttons, as in the example below:
 
 
 
