4506e88e7d1b1ed1c3bb66e82ba4fdaca9f0061e
SungGeun Kim <snugkim@gmail.com>
2017-07-18 20:09:59 -0700

    Update findSubstrings.py

--- codefight/findSubstrings.py
+++ codefight/findSubstrings.py
@@ -1,16 +1,21 @@
 '''
 ref:https://codefights.com/interview-practice/task/Ki9zRh5bfRhH6oBau
 
-You have two arrays of strings, words and parts. Return an array that contains the strings from words, modified so that any occurrences of the substrings from parts are surrounded by square brackets [], following these guidelines:
+You have two arrays of strings, words and parts. 
+Return an array that contains the strings from words, 
+modified so that any occurrences of the substrings from parts are surrounded by square brackets [], 
+following these guidelines:
 
-If several parts substrings occur in one string in words, choose the longest one. If there is still more than one such part, then choose the one that appears first in the string.
+If several parts substrings occur in one string in words, choose the longest one. 
+If there is still more than one such part, then choose the one that appears first in the string.
 
 Example
 
 For words = ["Apple", "Melon", "Orange", "Watermelon"] and parts = ["a", "mel", "lon", "el", "An"], the output should be
 findSubstrings(words, parts) = ["Apple", "Me[lon]", "Or[a]nge", "Water[mel]on"].
 
-While "Watermelon" contains three substrings from the parts array, "a", "mel", and "lon", "mel" is the longest substring that appears first in the string.
+While "Watermelon" contains three substrings from the parts array, 
+"a", "mel", and "lon", "mel" is the longest substring that appears first in the string.
 '''
 
 
