198cab8850c391e662d2c8f44a50fc4680b14397
SungGeun Kim <snugkim@gmail.com>
2017-07-19 09:59:42 -0700

    Update pressing_buttons.py

--- codefight/pressing_buttons.py
+++ codefight/pressing_buttons.py
@@ -1,3 +1,29 @@
+'''
+reference - https://codefights.com/interview-practice/task/SyBbdPYC86RdkKvjH
+Given a string of digits, return all of the possible non-empty letter combinations that the number could represent. The mapping of digits to letters is the same as you would find on a telephone's buttons, as in the example below:
+
+
+
+The resulting array should be sorted lexicographically.
+
+Example
+
+For buttons = "42", the output should be
+pressingButtons(buttons) = ["ga", "gb", "gc", "ha", "hb", "hc", "ia", "ib", "ic"].
+
+Input/Output
+
+[time limit] 4000ms (py3)
+[input] string buttons
+
+A string composed of digits ranging from '2' to '9'.
+
+Guaranteed constraints:
+
+0 ≤ buttons.length ≤ 7.
+
+[output] array.string
+'''
 def pressingButtons(buttons):
     mapping = {
         '1': '',
