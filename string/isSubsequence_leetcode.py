7c534bf8dbce60664baa361cfde0ba2403bd209f
SungGeun Kim <snugkim@gmail.com>
2017-07-25 21:39:37 -0700

    Update isSubsequence_leetcode.py

--- string/isSubsequence_leetcode.py
+++ string/isSubsequence_leetcode.py
@@ -37,3 +37,36 @@ class Solution(object):
                 j += 1
             if j == len(s): return True
         return False
+    
+'''Efficient Solution:
+
+from collections import defaultdict
+from bisect import bisect_left
+class Solution(object):
+    
+    def createMap(self, s):
+        # create a map. key is char. value is index of apperance in acending order. 
+        posMap = defaultdict(list)
+        for i, char in enumerate(s):
+            posMap[char].append(i)
+        return posMap
+        
+    
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        posMap = self.createMap(t)
+        # lowBound is the minimum index the current char has to be at.
+        lowBound = 0
+        for char in s:
+            if char not in posMap: return False
+            charIndexList = posMap[char]
+            # try to find an index that is larger than or equal to lowBound
+            i = bisect_left(charIndexList, lowBound)
+            if i == len(charIndexList): return False
+            lowBound = charIndexList[i] + 1
+        return True
+ '''
